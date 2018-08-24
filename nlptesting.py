3e1db1bbc875c9deb0175f34b5f2efea0991f848
JR <juhani.rantaniemi@gmail.com>
2018-08-24 11:15:10 +0300

    gutenberg

--- nlptesting.py
+++ nlptesting.py
@@ -6,3 +6,10 @@ This is a temporary script file.
 """
 
 #Testing nltk and open nlp syntax
+import nltk
+nltk.download('gutenberg')
+from nltk.corpus import gutenberg
+
+
+alice = gutenberg.raw(fileids = 'carroll-alice.txt')
+print (len(alice))
